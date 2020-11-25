69f2c3746aecbee9ee30f488e16d5a0fe95ebfb7
Rafael Alexandre <rafael.alexandre013@gmail.com>
2020-11-25 20:07:25 -0300

    Add more

--- dictionaries.py
+++ dictionaries.py
@@ -1,2 +1,10 @@
 # create a new dictionary
-house = {"Harry": "Gryffindor", "Draco":"Slyntherin"}
+houses = {"Harry": "Gryffindor", "Draco":"Slyntherin"}
+
+ages = {"Alice": 22, "Bob": 27}
+ages["Charlie"] = 30
+ages["Alice"] += 1
+
+print(ages)
+print("--------------------------")
+print(houses["Harry"])
