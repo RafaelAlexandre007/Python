ebff03d933c3c03eb10e7afb5704010802131565
Rafael Alexandre <rafael.alexandre013@gmail.com>
2020-11-25 19:57:57 -0300

    More loops.py

--- loops.py
+++ loops.py
@@ -1,6 +1,14 @@
+# Basic sintax for loops
+for i in [0, 1, 2, 3, 4, 5, 6]:
+    print(i)
+
 for i in range(5):
     print(i)
 
 names = ["Alice", "Bob", "Charlie"]
 for name in names:
     print(name)
+
+print("--------------------------------------")
+for i in names[0]:
+    print(i)
